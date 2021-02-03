fe9f9cbc369733aca19024dbec8ce4594309d2e5
Maxime Autechaud <maxime.autechaud@gmail.com>
2021-02-03 15:00:38 +0100

    creation entité User, userFixture (a remplir)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
